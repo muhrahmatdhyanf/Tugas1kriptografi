28a8c417ff4fe370433f9d70452241c26138ab46
muhammadrahmatdhyanf <77716550+muhrahmatdhyanf@users.noreply.github.com>
2022-12-26 21:08:12 +0800

    perubahan 2

--- encryption.py
+++ encryption.py
@@ -1,89 +1,12 @@
-
-
-# # # var = input("Masukan plainteks = ")
-# # # key = input("Masukan key berupa huruf = ")
-# # # binary = " ".join(map(bin,bytearray(var)))
-# # # binaryes = " ".join(format(ord(x), "b")for x in key)
-
-# # # # ts = binary ^ binaryes
-
-# # # print (binary)
-# # # print (binaryes)
-# # # # print (bin(ts))
-
-# # # # for var in range(key):
-# # # #     print(var)    
-
-# # # A = 0b1100
-# # # B = 0b0010
-
-# # # print(bin(A^B))
-
-# # # Python program to illustrate the
-# # # conversion of ASCII to Binary
-
-
-# # # Python program to illustrate the
-# # # conversion of ASCII to Binary
-
-# # # Calling string.encode() function to
-# # # turn the specified string into an array
-# # # of bytes
-# byte_array = input("masukan teks : ").encode()
-
-# # # Converting the byte_array into a binary
-# # integer
-# binary_int = int.from_bytes(byte_array,"big")
-
-# # Converting binary_int to a string of
-# # binary characters
-# binary_string = bin(binary_int)
-
-
-# byte_arrays = input("masukan key : ").encode()
-
-# # Converting the byte_array into a binary
-# # integer
-# binary_integer = int.from_bytes(byte_arrays,"big")
-
-# # Converting binary_int to a string of
-# # binary characters
-# binary_strings = bin(binary_integer)
-
-
-
-# ts = binary_int ^ binary_integer
-
-# # Getting the converted binary characters
-# print(bin(ts))
-
-
-# var  =  input("masukan plain teks = ")
-# key  =  input("masukan key = ")
-
-# binary = " ".join(format(ord(x), "b")for x in var )
-# binaryes = " ".join(format(ord(c), "b")for c in key )
-# normal = "".join(format(chr(int(c, 2))for c in var.split(" ")))
-
-# kali  = len(str(binary))
-
-# print (normal)
-# print(kali)
-
-# print (binary)
-# print (binaryes)
-# print (binary)
-# print (binaryes)
-
-
-
+print("MUHAMMAD RAHMAT DHYAN FAHRUDDIN")
+print("E1E120084")
 def generateKey(string, key): 
     key = list(key) 
-    if len(string) == len(string): 
+    if len(string) == len(key): 
         return(key) 
     else: 
-        for i in range(len(string) -len(string)): 
-            key.append(key[i % len(string)])
+        for i in range(len(string) -len(key)): 
+            key.append(key[i % len(key)])
     # print (binary)
     return("".join(key))
     
